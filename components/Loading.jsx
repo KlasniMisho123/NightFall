cf438a0810f29fab411513e5a6c115f02269f79b
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-16 15:50:22 +0400

    Loading commit

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div className='flex h-[100vh] bg-blue-300 items-center justify-center gap-2 '>
+    <div className='flex flex-col h-[100vh] bg-blue-300 items-center justify-center'>
+      <div class="flex gap-1 items-center">
         <div className="h-12 w-12 bg-yellow-500 rounded-full animation-delay-0"></div>
         <div className="h-12 w-12 bg-yellow-500 rounded-full animation-delay-1"></div>
         <div className="h-12 w-12 bg-yellow-500 rounded-full animation-delay-2"></div>
@@ -11,6 +12,17 @@ export default function Loading() {
         <div className="h-12 w-12 bg-yellow-500 rounded-full animation-delay-5"></div>
         <div className="h-12 w-12 bg-yellow-500 rounded-full animation-delay-6"></div>
         <div className="h-12 w-12 bg-yellow-500 rounded-full animation-delay-7"></div>
+      </div>
+      <div class="flex gap-1 items-center">
+        <div className="h-12 w-12 bg-yellow-500 rounded-full animation-delay-0"></div>
+        <div className="h-12 w-12 bg-yellow-500 rounded-full animation-delay-1"></div>
+        <div className="h-12 w-12 bg-yellow-500 rounded-full animation-delay-2"></div>
+        <div className="h-12 w-12 bg-yellow-500 rounded-full animation-delay-3"></div>
+        <div className="h-12 w-12 bg-yellow-500 rounded-full animation-delay-4"></div>
+        <div className="h-12 w-12 bg-yellow-500 rounded-full animation-delay-5"></div>
+        <div className="h-12 w-12 bg-yellow-500 rounded-full animation-delay-6"></div>
+        <div className="h-12 w-12 bg-yellow-500 rounded-full animation-delay-7"></div>
+      </div>
     </div>
   )
 }
