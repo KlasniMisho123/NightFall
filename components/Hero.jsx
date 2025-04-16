80276f7351415b2f0243650fab491a875932ae06
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-16 14:45:33 +0400

    project setup

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className="flex justify-center items-center h-[100vh] text-red-300 text-5xl font-bold animate-bounce"> Work In Process ... <i class="fa-solid fa-hammer"></i> </div>
+    <div className="flex justify-center items-center h-[100vh] text-red-300 text-5xl font-bold animate-bounce"> Work In Process ... <i className="fa-solid fa-hammer ml-4"></i> </div>
   )
 }
