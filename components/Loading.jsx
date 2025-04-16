d5079862533df19351428c0eda717c0cb040a0d9
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-16 15:44:20 +0400

    Loading commit

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div className='flex h-[100vh] bg-blue-300 items-center justify-center '>
-      <div class="flex gap-1 items-center h-full">
+    <div className='flex h-[100vh] bg-blue-300 items-center justify-center gap-2 '>
         <div class="h-12 w-12 bg-yellow-500 rounded-full animation-delay-0"></div>
         <div class="h-12 w-12 bg-yellow-500 rounded-full animation-delay-1"></div>
         <div class="h-12 w-12 bg-yellow-500 rounded-full animation-delay-2"></div>
@@ -12,8 +11,6 @@ export default function Loading() {
         <div class="h-12 w-12 bg-yellow-500 rounded-full animation-delay-5"></div>
         <div class="h-12 w-12 bg-yellow-500 rounded-full animation-delay-6"></div>
         <div class="h-12 w-12 bg-yellow-500 rounded-full animation-delay-7"></div>
-      </div>
-
     </div>
   )
 }
