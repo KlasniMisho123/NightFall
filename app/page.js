c0559f39c952cf535bce429bad791962ef6f614e
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-16 13:54:02 +0400

    AuthContext setup

--- app/page.js
+++ app/page.js
@@ -1,10 +1,6 @@
-import { useAuth } from "@/context/authcontext";
-
-
 export default function Home() {
-  [ authTest ] = useAuth();
 
   return (
-    <div className="flex justify-center items-center h-[100vh] text-red-300 text-5xl font-bold animate-bounce"> Work In Process ...</div>
+    <div className="flex justify-center items-center h-[100vh] text-red-300 text-5xl font-bold animate-bounce"> Work In Process ... </div>
   );
 }
