2625282691f4b0ea5f4b5d971649313254501262
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-16 13:38:36 +0400

    project setup

--- app/page.js
+++ app/page.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-[100vh] text-red-300 text-5xl font-bold"> Work In Process ...</div>
+    <div className="flex justify-center items-center h-[100vh] text-red-300 text-5xl font-bold animate-bounce"> Work In Process ...</div>
   );
 }
