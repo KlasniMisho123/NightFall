5509787e0f99605a94c25b2132917dc0d3333891
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-16 16:50:43 +0400

    Loading commit

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -3,6 +3,13 @@ import React from 'react'
 export default function Loading() {
   return (
     <div className='flex flex-col h-[100vh]  items-center justify-center'>
+      <div className="flex justify-center items-center text-red-500 text-5xl font-bold absolute top-12">
+        Work In Process 
+        <i className="fa-solid fa-hammer ml-4">
+          <span className="forge-animation"></span>
+        </i>
+      </div>
+
       <div className="flex gap-2 items-center loading-div-two">
         <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-0"></div>
         <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-1"></div>
