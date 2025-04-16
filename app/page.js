ca970685c52114761ef51503f18c57e956a56fa3
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-16 14:21:23 +0400

    project setup

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import Hero from "@/components/Hero";
-import { Main } from "next/document";
 
 export default function Home() {
 
@@ -7,7 +6,7 @@ export default function Home() {
     // <Main>
     //   <Hero />  
     // </Main>
-    <div className="flex justify-center items-center h-[100vh] text-red-300 text-5xl font-bold animate-bounce"> Work In Process ... </div>
-
+    // <div className="flex justify-center items-center h-[100vh] text-red-300 text-5xl font-bold animate-bounce"> Work In Process ... </div>
+    <Hero />
   );
 }
