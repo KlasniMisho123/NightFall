3f47f97c52ae897a95aac0f6c611630fbd064581
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-16 16:23:55 +0400

    Loading commit

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -3,6 +3,16 @@ import React from 'react'
 export default function Loading() {
   return (
     <div className='flex flex-col h-[100vh] bg-blue-300 items-center justify-center'>
+      <div className="flex gap-1 items-center loading-div-two">
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-0"></div>
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-1"></div>
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-2"></div>
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-3"></div>
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-4"></div>
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-5"></div>
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-6"></div>
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-7"></div>
+      </div>
       <div className="flex gap-2 items-center loading-div-one">
         <div className="h-8 w-8 bg-yellow-500 rounded-full firstDiv-animation-delay-0"></div>
         <div className="h-8 w-8 bg-yellow-500 rounded-full firstDiv-animation-delay-1"></div>
@@ -14,16 +24,6 @@ export default function Loading() {
         <div className="h-8 w-8 bg-yellow-500 rounded-full firstDiv-animation-delay-7"></div>
       </div>
 
-      <div className="flex gap-1 items-center loading-div-two">
-        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-0"></div>
-        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-1"></div>
-        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-2"></div>
-        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-3"></div>
-        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-4"></div>
-        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-5"></div>
-        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-6"></div>
-        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-7"></div>
-      </div>
 
     </div>
   )
