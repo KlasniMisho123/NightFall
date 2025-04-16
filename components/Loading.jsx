c4a43c3ef38d1f8017393ce947eb87a92bd6c10d
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-16 15:17:54 +0400

    Loading commit

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div className='flex h-[100vh] bg-blue-300 items-center justify-center gap-4'>
-      <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
-      <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
-      <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
-      <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
-      <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
-      <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
-      <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
-      <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
-      <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
+    <div className='flex h-[100vh] bg-blue-300 items-center justify-center '>
+      <div className='flex gap-1 loading-div-one'>
+        <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
+        <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
+        <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
+        <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
+        <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
+        <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
+        <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
+        <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
+        <div className='h-12 w-12 bg-yellow-500 rounded-full'></div>
+      </div>
     </div>
   )
 }
