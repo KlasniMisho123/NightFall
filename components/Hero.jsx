1c7025e4ecbe91d19ed80a1c17c3b12cb2b570b6
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-17 20:41:23 +0400

    loading commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className="flex justify-center items-center h-[100vh] text-red-300 text-5xl font-bold animate-bounce"> Work In Process ... <i className="fa-solid fa-hammer ml-4"></i> </div>
+    <div className='flex flex-col h-[100vh]  items-center justify-center'>
+      <div className="flex justify-center items-center text-red-500 text-5xl font-bold absolute top-12">
+        Work In Process 
+        <i className="fa-solid fa-hammer ml-4 forge-animation">
+        </i>
+      </div>
+
+      <div className="flex gap-2 items-center loading-div-two">
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-0"></div>
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-1"></div>
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-2"></div>
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-3"></div>
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-4"></div>
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-5"></div>
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-6"></div>
+        <div className="h-8 w-8 bg-red-500 rounded-full secondDiv-animation-delay-7"></div>
+      </div>
+      <div className="flex gap-2 items-center loading-div-one">
+        <div className="h-8 w-8 bg-blue-500 rounded-full firstDiv-animation-delay-0"></div>
+        <div className="h-8 w-8 bg-blue-500 rounded-full firstDiv-animation-delay-1"></div>
+        <div className="h-8 w-8 bg-blue-500 rounded-full firstDiv-animation-delay-2"></div>
+        <div className="h-8 w-8 bg-blue-500 rounded-full firstDiv-animation-delay-3"></div>
+        <div className="h-8 w-8 bg-blue-500 rounded-full firstDiv-animation-delay-4"></div>
+        <div className="h-8 w-8 bg-blue-500 rounded-full firstDiv-animation-delay-5"></div>
+        <div className="h-8 w-8 bg-blue-500 rounded-full firstDiv-animation-delay-6"></div>
+        <div className="h-8 w-8 bg-blue-500 rounded-full firstDiv-animation-delay-7"></div>
+      </div>
+    </div>
   )
 }
