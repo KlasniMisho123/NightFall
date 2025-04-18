187119b639241fff986a561efcb2d97e75662343
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-18 14:16:56 +0400

    Header commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className='flex justify-between bg-gray-600 px-4 py-2 cursor-pointer'>
       <div className='flex gap-2 items-center font-bold text-blue-400  moonLogoAnimation'>
-        <img  src='./moonlogo.png' alt='NightFall Home' className='max-h-12 max-w-12 shadow-lg shadow-sky-500/50 rounded-full'/>
+        <img  src='./moonlogo.png' alt='NightFall Home' className='max-h-12 max-w-12 shadow-lg shadow-sky-500/50 rounded-full moonImg'/>
         <span> NightFall </span>
       </div>
       <nav>NAVBAR</nav>
