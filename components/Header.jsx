62ab1fbc4e443d07f8440e2f501371941fc5def8
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-18 20:49:53 +0400

    sidenavbar commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -49,20 +49,27 @@ export default function Header() {
         >
           Contact us
           <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
-          <i className="fa-solid fa-rocket text-red-400 text-lg compassAnimation mt-1"></i> 
+          <i className="fa-solid fa-rocket text-red-400  text-lg compassAnimation mt-1"></i> 
           {/* onhover Rocker flies up */}
         </Link>
       </nav>
-        <div className='flex visible sm:invisible'
-          onClick={toggleSideNavBar}>
-          {isSideNavBarActive?
-           (<i className={`fa-solid fa-xmark text-lg transition-all transition-duration-300 text-red-500 relative z-2` + (isSideNavBarActive? "" : " invisible")}></i>):
-           (<i className={`fa-solid fa-bars text-lg transition-all transition-duration-300 text-gray-300 relative z-2` + (isSideNavBarActive? " invisible" : "")}></i>)
-          }
-        </div>
+      <div
+        className={
+          `flex cursor-pointer ` +
+          (isSideNavBarActive ? "visible" : "sm:invisible")
+        }
+        onClick={toggleSideNavBar}
+      >
+        {isSideNavBarActive ? (
+          <i className="fa-solid fa-xmark text-lg transition-all duration-300 text-red-500 relative z-5"></i>
+        ) : (
+          <i className="fa-solid fa-bars text-lg transition-all duration-300 text-gray-300 relative z-5"></i>
+        )}
+      </div>
+
         {isSideNavBarActive?
         (
-          <nav className='flex flex-col absolute w-full bg-gray-900 top-[10vh] left-0 z-50 text-white p-6 pb-12 gap-6'> 
+          <nav className='flex flex-col absolute w-full bg-gray-900 top-[9vh] left-0 z-50 text-white p-6 pb-12 gap-6'> 
           <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
             href='/'
             onClick={() => {setIsSideNavBarActive(false)}}>
@@ -74,7 +81,7 @@ export default function Header() {
           <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
             href='/projects'
             onClick={() => {setIsSideNavBarActive(false)}}>
-            <i className="fa-solid fa-star text-yellow-300"></i> 
+            <i className="fa-solid fa-star text-yellow-300 group-hover:animate-pulse"></i> 
             Projects
             <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
           </Link>
