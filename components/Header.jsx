b8a5b936c30811cf4227a175edf9bd4fc4332520
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-18 21:29:51 +0400

    sidenavbar commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -74,41 +74,48 @@ export default function Header() {
             (isSideNavBarActive ? 'max-h-[500px]' : 'max-h-0')
           }
         > */}
-        <nav className={'flex flex-col absolute bg-gray-900 w-full top-[9vh] left-0 z-50 text-white p-6 pb-12 gap-6 transition-all transition-duration-500 ease-in-out' + (isSideNavBarActive ? "max-h-auto opacity-100" : "max-h-0 opacity-0")}> 
-          <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
-            href='/'
-            onClick={() => {setIsSideNavBarActive(false)}}>
-            <i className="fa-solid fa-moon text-sky-400"></i>
-            Home
-            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
-          </Link>
-          
-          <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
-            href='/projects'
-            onClick={() => {setIsSideNavBarActive(false)}}>
-            <i className="fa-solid fa-star text-yellow-300 group-hover:animate-pulse"></i> 
-            Projects
-            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
-          </Link>
-          
-          <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
-            href='/aboutus'
-            onClick={() => {setIsSideNavBarActive(false)}}>
-            <i className="fa-solid fa-user-astronaut text-orange-300"></i>
-            About us
-            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
-          </Link>
-          
-          <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
-            href='/support'
-            onClick={() => {setIsSideNavBarActive(false)}}>
-            <i className="fa-solid fa-rocket text-red-400 text-lg compassAnimation mt-1"></i>
-            Contact us
-            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
-          </Link>
+        <nav
+          className={
+            'absolute w-full top-[9vh] left-0 z-50 bg-gray-900 text-white transition-all duration-500 ease-in-out overflow-hidden ' +
+            (isSideNavBarActive ? 'max-h-[500px]' : 'max-h-0')
+          }
+        >
+            <div className="flex flex-col p-6 pb-12 gap-6">
+              <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
+                href='/'
+                onClick={() => {setIsSideNavBarActive(false)}}>
+                <i className="fa-solid fa-moon text-sky-400"></i>
+                Home
+                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
+              </Link>
+              
+              <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
+                href='/projects'
+                onClick={() => {setIsSideNavBarActive(false)}}>
+                <i className="fa-solid fa-star text-yellow-300 group-hover:animate-pulse"></i> 
+                Projects
+                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
+              </Link>
+              
+              <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
+                href='/aboutus'
+                onClick={() => {setIsSideNavBarActive(false)}}>
+                <i className="fa-solid fa-user-astronaut text-orange-300"></i>
+                About us
+                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
+              </Link>
+              
+              <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
+                href='/support'
+                onClick={() => {setIsSideNavBarActive(false)}}>
+                <i className="fa-solid fa-rocket text-red-400 text-lg compassAnimation mt-1"></i>
+                Contact us
+                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
+              </Link>
+            </div>
         </nav>
 
-        {isSideNavBarActive?
+        {/* {isSideNavBarActive?
         (
           <nav className='flex flex-col absolute w-full bg-gray-900 top-[9vh] left-0 z-50 text-white p-6 pb-12 gap-6 '  > 
           <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
@@ -144,7 +151,7 @@ export default function Header() {
           </Link>
         </nav>
         ):("")
-       }
+       } */}
     </div>
   )
 }
