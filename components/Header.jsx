b5e37bd193d04e98d7b03b4ba521efcf8bcc0bbe
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-18 21:38:15 +0400

    sidenavbar commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Exo_2 } from 'next/font/google';
 import Link from 'next/link';
 
@@ -7,11 +7,21 @@ const exo2 = Exo_2({subsets: ["latin"],weight: ["400", "700"]});
 
 export default function Header() {
   const [isSideNavBarActive, setIsSideNavBarActive] = useState(false)
-
+  const [shouldRenderNav, setShouldRenderNav] = useState(false)
 
   function toggleSideNavBar() {
     setIsSideNavBarActive(!isSideNavBarActive)
   }
+
+  // if sidebar is not rendered, it will dismount from dom, but also allows to get open/close animation
+  useEffect(()=> {
+    if (isSideNavBarActive) {
+      setShouldRenderNav(true); 
+    } else {
+      const timeout = setTimeout(() => setShouldRenderNav(false), 500) 
+      return () => clearTimeout(timeout)
+    }
+  },[isSideNavBarActive])
 
   return (
     <div className='flex items-center justify-between bg-gray-600 px-4 pt-2 pb-3'>
@@ -67,14 +77,11 @@ export default function Header() {
           <i className="fa-solid fa-bars text-lg transition-all duration-200 text-gray-300 relative z-5 hover:text-white hover:scale-105"></i>
         )}
       </div>
-        {/* <nav
-          className={
-            'flex flex-col absolute bg-gray-900 w-full top-[9vh] left-0 z-50 text-white p-6 pb-12 gap-6 ' +
-            'transition-all duration-500 ease-in-out overflow-hidden ' +
-            (isSideNavBarActive ? 'max-h-[500px]' : 'max-h-0')
-          }
-        > */}
-        <nav
+        
+
+        {isSideNavBarActive?
+        (
+          <nav
           className={
             'absolute w-full top-[9vh] left-0 z-50 bg-gray-900 text-white transition-all duration-500 ease-in-out overflow-hidden ' +
             (isSideNavBarActive ? 'max-h-[500px]' : 'max-h-0')
@@ -114,44 +121,8 @@ export default function Header() {
               </Link>
             </div>
         </nav>
-
-        {/* {isSideNavBarActive?
-        (
-          <nav className='flex flex-col absolute w-full bg-gray-900 top-[9vh] left-0 z-50 text-white p-6 pb-12 gap-6 '  > 
-          <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
-            href='/'
-            onClick={() => {setIsSideNavBarActive(false)}}>
-            <i className="fa-solid fa-moon text-sky-400"></i>
-            Home
-            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
-          </Link>
-          
-          <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
-            href='/projects'
-            onClick={() => {setIsSideNavBarActive(false)}}>
-            <i className="fa-solid fa-star text-yellow-300 group-hover:animate-pulse"></i> 
-            Projects
-            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
-          </Link>
-          
-          <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
-            href='/aboutus'
-            onClick={() => {setIsSideNavBarActive(false)}}>
-            <i className="fa-solid fa-user-astronaut text-orange-300"></i>
-            About us
-            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
-          </Link>
-          
-          <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
-            href='/support'
-            onClick={() => {setIsSideNavBarActive(false)}}>
-            <i className="fa-solid fa-rocket text-red-400 text-lg compassAnimation mt-1"></i>
-            Contact us
-            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
-          </Link>
-        </nav>
-        ):("")
-       } */}
+        ):(<></>)
+       }
     </div>
   )
 }
