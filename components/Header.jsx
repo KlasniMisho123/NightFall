db34d2307df682031ae8db28788d1f624f85da71
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-18 14:08:08 +0400

    Header commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div className='flex justify-between bg-gray-600 px-4 py-2 cursor-pointer hover:moonLogoAnimation'>
-      <div className='flex gap-2 items-center font-bold text-blue-400'>
+    <div className='flex justify-between bg-gray-600 px-4 py-2 cursor-pointer'>
+      <div className='flex gap-2 items-center font-bold text-blue-400  moonLogoAnimation'>
         <img  src='./moonlogo.png' alt='NightFall Home' className='max-h-12 max-w-12 shadow-lg shadow-sky-500/50 rounded-full'/>
         <span> NightFall </span>
       </div>
