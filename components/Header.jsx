1b682d01a60a4bc27fb827fb189a97729d335435
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-18 13:40:18 +0400

    Header commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='flex justify-between bg-green-300 px-4 py-2'>
+      <div>LOGO DIV</div>
+      <nav>NAVBAR</nav>
+      <div></div>
+    </div>
   )
 }
