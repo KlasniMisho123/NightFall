ccf927c5481a2d27d1f4d7700c9828dfbf60429d
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-18 13:53:34 +0400

    Header commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div className='flex justify-between bg-gray-600 px-4 '>
+    <div className='flex justify-between bg-gray-600 px-4 py-2 '>
       <div>
-        <img  src='./moonlogo-smbg.png' alt='NightFall Home' className='h-16 w-24 shadow-sm shadow-blue-500/50 rounded-full'/>
+        <img  src='./moonlogo.png' alt='NightFall Home' className='h-12 w-12 shadow-lg shadow-sky-500/50 rounded-full'/>
       </div>
       <nav>NAVBAR</nav>
       <div></div>
