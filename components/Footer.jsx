244e3cf88d2aa82712ecf7f6753cf5f05e3afcf2
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-19 15:11:44 +0400

    footer commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer className='flex flex-col py-6 px-4 bg-gray-600 rounded-t '>
+      <div>1</div>
+      <div>2</div>
+      <div>© Copyright 2025, All Rights Reserved by Nightfall webWorks</div>
+    </footer>
   )
 }
--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <div>
         {/* <div className='h-[50vh] w-full bg-[url("https://i.pinimg.com/736x/2e/f1/7f/2ef17f9924b3be82864588523ad4c940.jpg")] bg-cover bg-center bg-no-repeat'> */}
-        <div className='h-[50vh] w-full bg-gray-900'>
+        <div className='w-full bg-gray-900'>
         </div>
     </div>
   )
