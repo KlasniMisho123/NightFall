249bea2c1dcdf9e41be88ea65540d271127fba56
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-23 14:20:45 +0400

    selectedNavSection

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,11 +8,16 @@ const exo2 = Exo_2({subsets: ["latin"],weight: ["400", "700"]});
 export default function Header() {
   const [isSideNavBarActive, setIsSideNavBarActive] = useState(false)
   const [shouldRenderNav, setShouldRenderNav] = useState(false)
+  const [selectedNavSection, setSelectedNavSection] = useState('home')
 
   function toggleSideNavBar() {
     setIsSideNavBarActive(!isSideNavBarActive)
   }
 
+  function handleSelectedNavSection(section) {
+    setSelectedNavSection(section)
+    setIsSideNavBarActive(false)
+  }
   // if sidebar is not rendered, it will dismount from dom, but also allows to get open/close animation
   useEffect(()=> {
     if (isSideNavBarActive) {
@@ -27,7 +32,9 @@ export default function Header() {
     <div className='flex items-center justify-between bg-gray-600 px-4 pt-2 pb-3'>
       <Link  className='flex gap-2 items-center font-bold text-blue-400  moonLogoAnimation cursor-pointer'
       href='/'
-      onClick={() => {setIsSideNavBarActive(false)}}
+      onClick={() => {
+        handleSelectedNavSection('home')
+      }}
       >
         <img  src='./moonlogo.png' alt='NightFall Home' className='max-h-12 max-w-12 shadow-lg shadow-sky-500/50 rounded-full moonImg'/>
         <span className={' ' + exo2.className}> NightFall </span>
@@ -35,7 +42,7 @@ export default function Header() {
       <nav className={'flex whitespace-nowrap gap-2 w-0 invisible md:gap-4 lg:gap-4 lg:ml-[-10%] sm:visible sm:w-auto ' + exo2.className}>
         <Link className="flex gap-1 items-center px-1  py-1 text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium"
         href='/'
-        onClick={() => {setIsSideNavBarActive(false)}}
+        onClick={() => {handleSelectedNavSection('home')}}
         >
         <i className="fa-solid fa-house"></i>
         Home
@@ -44,7 +51,7 @@ export default function Header() {
 
         <Link className="flex gap-1 items-center px-1  py-1 text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium"
         href='/projects'
-        onClick={() => {setIsSideNavBarActive(false)}}
+        onClick={() => {handleSelectedNavSection('projects')}}
         >
           Projects
           <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
@@ -52,7 +59,7 @@ export default function Header() {
 
         <Link className="flex gap-1 items-center px-1  py-1 text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium"
         href='/aboutus'
-        onClick={() => {setIsSideNavBarActive(false)}}
+        onClick={() => {handleSelectedNavSection('aboutus')}}
         >
           About us
           <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
@@ -60,7 +67,7 @@ export default function Header() {
 
         <Link className="flex gap-1 items-center px-1 py-1 text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium rocket-flies"
         href='/support'
-        onClick={() => {setIsSideNavBarActive(false)}}
+        onClick={() => {handleSelectedNavSection('support')}}
         >
           Contact us
           <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
@@ -94,7 +101,7 @@ export default function Header() {
             <div className="flex flex-col p-6 pb-12 gap-6">
               <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
                 href='/'
-                onClick={() => {setIsSideNavBarActive(false)}}>
+                onClick={() => {handleSelectedNavSection('home')}}>
                 <i className="fa-solid fa-moon text-sky-400"></i>
                 Home
                 <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
@@ -102,7 +109,7 @@ export default function Header() {
               
               <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
                 href='/projects'
-                onClick={() => {setIsSideNavBarActive(false)}}>
+                onClick={() => {handleSelectedNavSection('projects')}}>
                 <i className="fa-solid fa-star text-yellow-300 group-hover:animate-pulse"></i> 
                 Projects
                 <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
@@ -110,7 +117,7 @@ export default function Header() {
               
               <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4"
                 href='/aboutus'
-                onClick={() => {setIsSideNavBarActive(false)}}>
+                onClick={() => {handleSelectedNavSection('aboutus')}}>
                 <i className="fa-solid fa-user-astronaut text-orange-300"></i>
                 About us
                 <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
@@ -118,7 +125,7 @@ export default function Header() {
               
               <Link className="flex gap-2 items-center text-blue-400 hover:text-blue-500 relative group cursor-pointer font-medium border-b border-gray-700 p-4 "
                 href='/support'
-                onClick={() => {setIsSideNavBarActive(false)}}>
+                onClick={() => {handleSelectedNavSection('support')}}>
                 <i className="fa-solid fa-rocket text-red-400 text-lg compassAnimation mt-1"></i>
                 Contact us
                 <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-500 opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300"></span>
