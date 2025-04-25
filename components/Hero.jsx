b5721c6f87feabeb15a116aaa6de4b30c6e93467
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-25 12:41:30 +0400

    hero buttons commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,36 +6,38 @@ const urbanist = Urbanist({ subsets: ['latin'], weight: ['400', '700'] });
 
 export default function Hero() {
   return (
-    <div className='h-[100vh] px-12 py-4 bg-[#A7C6DA] '>
-      <div className='flex justify-between my-4'>
-        <div className='min-w-[60%] '> 
+    <div className='h-[100vh] py-4 bg-[#A7C6DA] px-2 md:px-18 lg:px-28 '>
+      <div className='flex gap-2 justify-between my-6'>
+        <div className='min-w-none md:min-w-[60%] '> 
           {/* HERO CARD */}
           {/* Background color */}
           {/* ROCKET -!? FLIES AND LEAVES MOTTO? */}
-          {/* Dummy Text */}
           <span className={'border-4 rounded-lg px-4 py-2 my-4 inline-block border-blue-500/80 text-blue-500/90 font-bold rotate-[-5deg] text-sm md:text-base ' + urbanist.className }> Your partner in digital transformation</span> 
-          <h1 className={`text-white text-4xl my-4 sm:text-6xl font-bold mt-2 ` + urbanist.className}>
+          <h1 className={`text-white text-3xl my-4 md:text-4xl lg:text-6xl font-bold mt-2 whitespace-nowrap ` + urbanist.className}>
             Digitize Your Idea
           </h1>
 
-          <p className="text-gray-50 text-sm sm:text-base max-w-md mt-4">
-            Nightfall Webworks helps you build modern, responsive websites that elevate your brand and grow your digital presence.
-             From concept to launch — we code your vision into reality.
+          <p className="text-gray-50 text-sm max-w-md mt-3 leading-relaxed md:text-base ">
+          Nightfall Webworks helps you build modern, responsive websites that elevate your brand and grow your digital presence.
+          From concept to launch — we code your vision into reality.
           </p>
 
-          <div className='flex gap-2 my-4'>
-            <button className='border-2 p-2 px-4 rounded-lg text-white bg-blue-500 border-blue-500 hover:border-white hover:brightness-90 transition-all duration-300 cursor-pointer shadow-sm '>Contact Us</button>
-            {/* bg-[#213448] */}
-            <button className="border-2 p-2 px-4 rounded-lg text-white border-blue-500 hover:text-blue-500  transition-all duration-300 cursor-pointer shadow-sm ">
-  About Us
-</button>
 
+          <div className="flex my-6 gap-4 sm:gap-[10%]">
+            <button className="flex-1 max-w-40 border-2 p-3 rounded-lg text-white bg-blue-500 border-blue-500 hover:border-white hover:brightness-90 transition-all duration-300 cursor-pointer shadow-sm whitespace-nowrap">
+              Contact Us
+            </button>
+            <button className="flex-1 max-w-40 border-2 p-3 rounded-lg text-white border-blue-500 hover:text-blue-500 transition-all duration-300 cursor-pointer shadow-sm whitespace-nowrap">
+              About Us
+            </button>
           </div>
+
+
           <section>
             Stat DIV?
           </section>
         </div>
-        <div className='min-w-[35%] border-2 border-gray-800'> Hero </div>
+        <div className='min-w-none md:min-w-[35%] border-2 border-gray-800'> Hero </div>
         {/* <div> ANIMATION</div> */}
       </div>
         <section> OUR SERVICES </section>
