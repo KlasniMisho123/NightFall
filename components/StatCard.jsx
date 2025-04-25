f3416c8add6d54400f11c2f35da979d7c6b288fd
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-25 20:01:43 +0400

    stat card

--- components/StatCard.jsx
+++ components/StatCard.jsx
@@ -4,11 +4,10 @@ export default function StatCard(props) {
   const { color } = props
 
   return (
-    
         <div className='flex flex-col gap-2 items-center '> 
-          <div className={'border-6 rounded w-12 border-' + color }> </div>
-          <div className={'font-bold text-4xl text-' + color }> 100+ </div>
-          <div className={'font-bold text-lg text-' + color+'/50'}> Years Of Service </div>
+          <div className={`border-6 rounded w-12 border-${color}`}></div>
+          <div className={`font-bold text-4xl text-${color}`}>100+</div>
+          <div className={`font-bold text-lg text-${color}/50`}>Years Of Service</div>
           <div className=''> To Monitor Mood </div>
         </div>
   )
