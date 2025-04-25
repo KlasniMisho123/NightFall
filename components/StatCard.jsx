9953d5507b1a041b0e25939f68e7e030659f010e
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-25 20:16:15 +0400

    statCard

--- components/StatCard.jsx
+++ components/StatCard.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
-export default function StatCard(props) {
-  const { color } = props
-
+export default function StatCard({color}) {
   return (
         <div className='flex flex-col gap-2 items-center '> 
-          <div className={`border-6 rounded w-12 border-${color}`}></div>
+          <p className='text-red-500'> border-{color}</p>
+          <div className={` border-6 rounded w-12 ` + 'border-'+(color) }></div>
           <div className={`font-bold text-4xl text-${color}`}>100+</div>
           <div className={`font-bold text-lg text-${color}`}>Years Of Service</div>
           <div className=''> To Monitor Mood </div>
