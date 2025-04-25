2066d7e584e98d14b343f51fd438f21d6df6a92a
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-25 13:19:04 +0400

    hero buttons commit

--- components/StatCard.jsx
+++ components/StatCard.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function StatCard() {
   return (
-    <div>StatCard</div>
+    <div>
+        <div className='border-8 border-black'> 100 </div>
+        <div className='border-8 border-black'> 100 </div>
+        <div className='border-8 border-black'> 100 </div>
+    </div>
   )
 }
