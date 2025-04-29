e2993a9c9bdd2f27518fa77bbb7163b321696079
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-04-29 12:22:18 +0400

    flip a card

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -97,16 +97,17 @@ export default function Hero() {
             </div>
         </div>
           <div className="flex flex-col bg-white text-black gap-8 p-4 rounded cursor-pointer
-            transition-all transition-duration-300 transition-ease-in-out hover:scale-105">
+            transition-all transition-duration-300 transition-ease-in-out hover:scale-105 hover:rotate-y-180">
             <div className="flex justify-center">
                 <img src="websitesWallpaper.jpg" className="h-[200px] w-full " alt="" />
             </div>
-            <div className="flex flex-col items-center font-bold lg:items-start">
+            <div className="flex flex-col items-center font-bold lg:items-start ">
                 <h3 className="text-lg text-gray-600 text-center lg:text-start text-2xl lg:text-2xl ">Custom Websites</h3>
                 <div className="flex justify-between  my-4 w-full services-item-card-three">
                     <h3 className="text-xl">Bespoke designs built for your brand.</h3>
                 </div>
             </div>
+            {/* HOVER ANIMATION, Card flip */}
           </div>
 
           <div className="flex flex-col bg-white text-black gap-8 p-4 rounded cursor-pointer
