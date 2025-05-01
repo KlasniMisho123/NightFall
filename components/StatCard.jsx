3f8628179fe8fca9228ec61561188e83d7ea9957
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-01 14:54:09 +0400

    homepage commit

--- components/StatCard.jsx
+++ components/StatCard.jsx
@@ -9,9 +9,9 @@ export default function StatCard(props) {
 
   return (
     <div className='flex flex-col gap-4 items-center px-1 py-4 min-w-[100px] totalStatsOne md:px-2 lg:px-4 '>
-      <div className='border-1 border-blue-500  bg-blue-500 px-4 py-1 rounded-lg '> </div>   
-      <h2 className={'text-4xl text-blue-500 italic ' + mono.className}> {stat} </h2>
-      <h3 className={'text-sm text-blue-500 ' + poppins.className} > {title}: {cardColor}</h3>
+      <div className={`border-1 px-4 py-1 rounded-lg ` + `bg-${cardColor} border-${cardColor}`}> </div>   
+      <h2 className={`text-4xl text-blue-500 italic ` + `text-${cardColor} ` + mono.className}> {stat} </h2>
+      <h3 className={`text-sm  ` + `text-${cardColor}` + poppins.className} > {title}: {cardColor}</h3>
       <h4 className={'text-xs '}> {titleDec} </h4>
    </div>
   )
