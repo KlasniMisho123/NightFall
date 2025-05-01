f5909a1ac9a6c53eaf59f212005d8357ad468987
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-01 14:41:25 +0400

    homepage commit

--- components/StatCard.jsx
+++ components/StatCard.jsx
@@ -5,18 +5,15 @@ const poppins = Poppins({ subsets: ["latin"], weight: ["700"] });
 const mono = Space_Mono({ subsets: ["latin"], weight: ["700"] });
 
 export default function StatCard(props) {
-  const { color } = props;
-// same title color or -100
-// colors?
-// animation on load totalStatsOne ... 
-// RESPONSIVE
+  const {color, stat, title, titleDec } = props;
+
   return (
-    <div className='flex flex-col gap-4 items-center px-0  py-4 min-w-[150px] totalStatsOne md:px-2 lg:px-4 '>
-      <div className='border-1 border-blue-500  bg-blue-500 px-4 py-1 rounded-lg '> </div>   
-      <h2 className={'text-4xl text-blue-500 italic ' + mono.className}> 300+ </h2>
-      <h3 className={'text-sm text-blue-400 ' + poppins.className} > USERS </h3>
-      <h4 className={'text-xs '}> To Monitor Mood </h4>
-    </div>
+    <div className='flex flex-col gap-4 items-center px-1 py-4 min-w-[100px] totalStatsOne md:px-2 lg:px-4 '>
+    <div className='border-1 border-blue-500  bg-blue-500 px-4 py-1 rounded-lg '> </div>   
+    <h2 className={'text-4xl text-blue-500 italic ' + mono.className}> {stat} </h2>
+    <h3 className={'text-sm text-blue-500 ' + poppins.className} > {title} </h3>
+    <h4 className={'text-xs '}> {titleDec} </h4>
+  </div>
   )
 }
 
