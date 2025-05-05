9212a1113dbccdaeca9fc3cfa10966876a338139
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-05 23:45:25 +0400

    how it works commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,6 +5,7 @@ import { Poppins, Space_Mono, Urbanist } from 'next/font/google';
 import StatCard from './statCard';
 import Link from 'next/link';
 import ServiceCard from './ServiceCard';
+import HowItWorks from './HowItWorks';
 
 const urbanist = Urbanist({ subsets: ['latin'], weight: ['400', '700'] });
 const poppins = Poppins({ subsets: ["latin"], weight: ["700"] });
@@ -149,7 +150,9 @@ export default function Hero() {
             </div>
           </section>
         <span className={'border-4 rounded-lg px-4 py-2 my-4 inline-block border-blue-500/80 text-blue-500/90 font-bold rotate-[5deg] text-sm md:text-base ' + urbanist.className }>
-         How it Works</span> 
+         How it Works
+        </span> 
+        <HowItWorks />
     </div>
   )
 }
