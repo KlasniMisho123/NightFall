685f15967d0668db0ce32549cdec89d73e0e5f1f
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-05 23:54:01 +0400

    how it works commit

--- components/WorkStages.jsx
+++ components/WorkStages.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 
 export default function WorkStages() {
   return (
-    <section className='flex justify-evenly bg-pink-300'>
-      <div>
-        <span> Work Process </span>
-        <h2> How It <span> Works ?</span></h2>
-        <p>lorem inpsum</p>
+    <section className='flex justify-center gap-8 bg-green-100'>
+      <div className='flex flex-col gap-4 bg-blue-100'>
+        <div className='my-4'>
+          <span> Work Process </span>
+          <h2> How It <span> Works ?</span></h2>
+          <p>lorem inpsum</p>
+        </div>
+        <div className='my-4'>
+          <div>req1</div>
+          <div>req2</div>
+          <div>req3</div>
+          <div>req4</div>
+        </div>
       </div>
-      <div className=''>
+      <div className='bg-red-100 h-full'>
         {/* STAGE CARDS */}
         <div>1</div>
         <div>2</div>
