4503865b708ff5fe63cb139e5f7043317362e257
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-09 21:06:21 +0400

    Our Advantages

--- components/ServicesAnimation.jsx
+++ components/ServicesAnimation.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 export default function ServicesAnimation() {
   return (
-    <section className='bg-green-700'>
-        <span className='text-white'> NightFall/Services -Animation </span>
-
+    <section className='flex gap-4 border-2 border-blue-700 rounded px-2 py-4 '>
+        {/* <span className='text-white'> NightFall/Services -Animation </span> */}
+        <div className='bg-green-400 h-[300px] w-[300px]'>1</div>
+        <div className='bg-yellow-400 h-[300px] w-[300px]'>2</div>
+        <div className='bg-red-400 h-[300px] w-[300px]'>3</div>
+        <div className='bg-blue-400 h-[300px] w-[300px]'>4</div>
+        <div className='bg-purple-400 h-[300px] w-[300px]'>5</div>
     </section>
   )
 }
