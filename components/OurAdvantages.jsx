59da3d737b11acb0dfb9a268a7af7813af560489
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-09 21:16:55 +0400

    Our Advantages

--- components/OurAdvantages.jsx
+++ components/OurAdvantages.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 export default function OurAdvantages() {
   return (
-    <section className='flex gap-4 border-2 border-blue-700 rounded px-2 py-4 '>
-        {/* <span className='text-white'> NightFall/Services -Animation </span> */}
-        <div className='bg-green-400 h-[300px] w-[300px]'>1</div>
-        <div className='bg-yellow-400 h-[300px] w-[300px]'>2</div>
-        <div className='bg-red-400 h-[300px] w-[300px]'>3</div>
-        <div className='bg-blue-400 h-[300px] w-[300px]'>4</div>
-        <div className='bg-purple-400 h-[300px] w-[300px]'>5</div>
+    <section className="flex gap-4 border-2 border-blue-700 rounded px-2 py-4 overflow-x-auto">
+      <div className="bg-green-400 h-[300px] w-[250px] rounded flex flex-col items-center justify-center text-white">
+        <i className="fa-solid fa-crosshairs text-3xl mb-4"></i>
+        <h3 className="text-xl font-bold">Title</h3>
+      </div>
+      <div className="bg-yellow-400 h-[300px] w-[250px] rounded flex items-center justify-center text-xl font-bold text-white">
+        2
+      </div>
+      <div className="bg-red-400 h-[300px] w-[250px] rounded flex items-center justify-center text-xl font-bold text-white">
+        3
+      </div>
+      <div className="bg-blue-400 h-[300px] w-[250px] rounded flex items-center justify-center text-xl font-bold text-white">
+        4
+      </div>
+      <div className="bg-purple-400 h-[300px] w-[250px] rounded flex items-center justify-center text-xl font-bold text-white">
+        5
+      </div>
     </section>
-  )
+  );
 }
