3b21ef64681d918d756f27fee287417077bb4d4a
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-10 17:26:57 +0400

    advantages card dmeo

--- components/OurAdvantages.jsx
+++ components/OurAdvantages.jsx
@@ -7,64 +7,42 @@ const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 
 const advantageCardArray = [
   {
-    title: "Secure",
+    title: "Private",
     icon: <i className="fa-solid fa-shield-halved"></i>,
-    color: "text-blue-600"
-  },
+    color: ""
+  }, 
   {
-    title: "Clean",
+    title: "Simple",
     icon: <i className="fa-solid fa-egg"></i>,
-    color: "text-gray-600"
+    color: ""
   },
   {
-    title: "Custom",
+    title: "Targeted",
     icon: <i className="fa-solid fa-bullseye"></i>,
-    color: "text-red-500"
+    color: ""
   },
   {
-    title: "Creative",
+    title: "Insightful",
     icon: <i className="fa-solid fa-lightbulb"></i>,
-    color: "text-yellow-500"
+    color: ""
   },
   {
-    title: "Bold",
+    title: "Engaging",
     icon: <i className="fa-brands fa-sketch"></i>,
-    color: "text-indigo-500"
-  },
+    color: ""
+  }, 
   {
-    title: "Trusted",
+    title: " User Friendly ",
     icon: <i className="fa-solid fa-users"></i>,
-    color: "text-green-600"
-  },
-  {
-    title: "Fast",
-    icon: <i className="fa-solid fa-bolt"></i>,
-    color: "text-orange-500"
-  },
-  {
-    title: "Modern",
-    icon: <i className="fa-solid fa-display"></i>,
-    color: "text-cyan-500"
-  },
-  {
-    title: "Responsive",
-    icon: <i className="fa-solid fa-mobile-screen-button"></i>,
-    color: "text-purple-600"
-  },
-  {
-    title: "SEO",
-    icon: <i className="fa-solid fa-chart-line"></i>,
-    color: "text-emerald-500"
-  }
-];
-
-
+    color: ""
+  }, 
+]
 
 export default function OurAdvantages() {
   return (
     <div className='py-8 border border-gray-200 flex flex-col justify-center items-center gap-4 min-w-[430px] overflow-hidden '>
         <h1 className={'text-center text-lg lg:text-2xl textGradient text-white ' + fugaz.className}> Our Advantages </h1>
-        <div className='flex flex-row py-4 overflow-hidden '> 
+        <div className='flex flex-row py-4 overflow-hidden px-8 '> 
         {advantageCardArray.map((card, index) => ( 
             <div className='flex-shrink-0 w-1/3' key={index}> 
                 <AdvantagesCard
