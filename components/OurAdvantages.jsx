92750cd0938ff5fbe28b6b95c9232d583a842a9e
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-10 16:57:32 +0400

    advantages card

--- components/OurAdvantages.jsx
+++ components/OurAdvantages.jsx
@@ -1,15 +1,51 @@
-import { Fugaz_One } from 'next/font/google';
 import React from 'react'
+import AdvantagesCard from './AdvantagesCard'
+import { Fugaz_One } from 'next/font/google';
 
 
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 
-export default function AdvantagesCard(props) {
-    const {icon, title } = props
+const advantageCardArray = [
+  {
+    title: "Private",
+    icon: <i className="fa-solid fa-shield-halved"></i>
+  }, 
+  {
+    title: "Simple",
+    icon: <i className="fa-solid fa-egg"></i>
+  },
+  {
+    title: "Targeted",
+    icon: <i className="fa-solid fa-bullseye"></i>
+  },
+  {
+    title: "Insightful",
+    icon: <i className="fa-solid fa-lightbulb"></i>
+  },
+  {
+    title: "Engaging",
+    icon: <i className="fa-brands fa-sketch"></i>
+  }, 
+  {
+    title: " User Friendly ",
+    icon: <i className="fa-solid fa-users"></i>
+  }, 
+]
+
+export default function OurAdvantages() {
   return (
-    <div className='flex flex-col gap-8 rounded-lg border border-gray-300 bg-indigo-400 p-2 items-center justify-center w-[150px] h-[200px] advantageCardAction '>
-        <div className={`text-4xl text-gray-100 `}>{icon}</div>
-        <h1 className={'text-gray-100  ' + fugaz.className}> {title} </h1>
+    <div className='py-8 border border-gray-200 flex flex-col justify-center items-center gap-4 min-w-[430px] overflow-hidden '>
+        <h1 className={'text-center text-base lg:text-lg textGradient ' + fugaz.className}> Our Advantages </h1>
+        <div className='flex flex-row py-4 overflow-hidden '> 
+        {advantageCardArray.map((card, index) => ( 
+            <div className='flex-shrink-0 w-1/3' key={index}> 
+                <AdvantagesCard 
+                    title={card.title}
+                    icon={card.icon}
+                />
+            </div>
+        ))}
+        </div>
     </div>
   )
 }
