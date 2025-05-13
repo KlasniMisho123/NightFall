d010ea84ca9a5b6bfb2bb72e6f4dd3279e4148c4
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-13 21:49:21 +0400

    about us commit

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,7 +1,16 @@
+import { Urbanist } from 'next/font/google';
 import React from 'react'
+
+const urbanist = Urbanist({ subsets: ['latin'], weight: ['400', '700'] });
 
 export default function Projects() {
   return (
-    <div className='min-h-[100vh]' >Projects Page</div>
+    <div className='min-h-[100vh] py-4 px-4 md:px-14 lg:px-28 transition-all transition-duration-300 bg-nightfall' >
+      <section className='my-10 mx-8'>
+        <span className={'border-4 rounded-lg px-4 py-2 my-4 inline-block border-purple-500/80 text-purple-500/90 font-bold rotate-[-5deg] text-sm md:text-base ' + urbanist.className }>
+          Your partner in digital transformation
+        </span> 
+      </section>
+    </div>
   )
 }
