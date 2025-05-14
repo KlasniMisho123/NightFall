87e6c472e0a4d41822531857c615624b4ecf968e
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-14 19:52:37 +0400

    projects textstyl commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -60,12 +60,24 @@ export default function Hero() {
         </div>
         {/* Consider other Variatons for This Animation// (moon/sun ..env) */}
         <section className='min-w-0 sm:min-w-[20%] md:min-w-[25%] text-white main-hero-load flex items-center'>
-          <div className='flex flex-col items-center p-8'> 
+          <div className='flex flex-col items-center p-8 hero-fade-in'>
             <div className='flex justify-center gap-2'>
-              <img src='sun.png' alt='Sun icon' className='h-24 w-24 object-cover ' />
-              <img src='moonlogo.png' alt='Moon logo' className='h-24 w-24 object-cover' />
+              <img
+                src='sun.png'
+                alt='Sun icon'
+                className='h-24 w-24 object-cover spin-slow'
+              />
+              <img
+                src='moonlogo.png'
+                alt='Moon logo'
+                className='h-24 w-24 object-cover fade-delay'
+              />
             </div>
-            <img src='Mountains.png' alt='Mountain landscape' className='mt-4 object-cover' />
+            <img
+              src='Mountains.png'
+              alt='Mountain landscape'
+              className='mt-4 object-cover rise-up'
+            />
           </div>
         </section>
 
