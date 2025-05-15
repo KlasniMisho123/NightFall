ff0a416bb7f8550213a03f7241fd986a0d77843c
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-15 23:02:16 +0400

    projects commit

--- components/BorderedTitle.jsx
+++ components/BorderedTitle.jsx
@@ -4,15 +4,14 @@ import { Urbanist } from 'next/font/google';
 const urbanist = Urbanist({ subsets: ['latin'], weight: ['400', '700'] });
 
 export default function BorderedTitle(props) {
-  const { text, color = 'purple-500', angle} = props;
+  const { text, color = 'purple-500', posAngle} = props;
 
-  const rotateClass = `rotate-[${angle}deg]`;
   const borderColorClass = `border-${color}/80`;
   const textColorClass = `text-${color}/90`;
 
   return (
     <span
-      className={`border-4 rounded-lg px-4 py-2 my-4 inline-block font-bold text-sm md:text-base ${rotateClass} ${borderColorClass} ${textColorClass} ${urbanist.className}`}
+      className={`border-4 rounded-lg px-4 py-2 my-4 inline-block font-bold text-sm md:text-base ${borderColorClass} ${textColorClass} ${urbanist.className} ` + (posAngle ? "rotate-[5deg] " : "rotate-[-5deg] ")}
     >
       {text}
     </span>
