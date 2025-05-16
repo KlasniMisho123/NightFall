2b6fc19a11a6c3fcde274325c601d1408837964a
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-16 14:25:35 +0400

    Projects Layout

--- components/ProjectLayout.jsx
+++ components/ProjectLayout.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function ProjectLayout() {
   return (
-    <div>ProjectLayout</div>
+    <section className='my-12 flex bg-[#091826] justify-center gap-8 w-full text-white'>
+        <div className='flex flex-col '>
+            1
+        </div>
+        <div className='flex flex-col mt-5'>
+            2
+        </div>
+    </section>
   )
 }
