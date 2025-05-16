9fabb4f4efda4e05a6259eb6e54d5d0760ba19c1
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-17 00:00:13 +0400

    project card commit

--- components/ProjectLayout.jsx
+++ components/ProjectLayout.jsx
@@ -8,7 +8,7 @@ export default function ProjectLayout() {
         <div className='flex flex-col '>
              {/*  year, title, webLink, projectDesc, projectSpanColor, client, time, service, imgSrc */}
             <ProjectCard year={2024} projectName="MoodCheck" title=" - Track your mood and see how you feel over time" webLink="https://mood-check.vercel.app/" projectSpanColor="indigo-500" client="Developeing Skills"
-            time="10 Days" service="Web Page Development" imgSrc="Moodcheck.png"
+            time="10 Days" service="Web Page Development" imgSrc="MoodCheck.png"
             
             projectDesc="is a personal mood tracking application that allows you to rate your day using a 5-point scale, add a short description or reflection, 
             and save your entries securely to a Firebase server tied to your personal account. The app provides a simple and intuitive interface for logging daily moods,
@@ -17,7 +17,14 @@ export default function ProjectLayout() {
             Whether you're tracking for personal insight or simply want a fun way to reflect on your feelings, MoodCheck offers a thoughtful and user-friendly experience." />
         </div>
         <div className='mt-16'>
-            <ProjectCard />
+            <ProjectCard year={2024} projectName="EspressoMeter" title=" Coffee Tracking for Cofee Fiends" webLink="https://espresso-meter.vercel.app/" projectSpanColor="indigo-500" client="Developeing Skills"
+            time="2 Weeks" service="Web Page Development" imgSrc="EspressoMeter.png"
+            
+            projectDesc="is a personal mood tracking application that allows you to rate your day using a 5-point scale, add a short description or reflection, 
+            and save your entries securely to a Firebase server tied to your personal account. The app provides a simple and intuitive interface for logging daily moods,
+            helping you stay mindful of your emotional well-being over time. In addition to mood logging, MoodCheck includes extra features such as mood statistics to 
+            help you visualize patterns or changes in your emotional state, as well as a section for light-hearted jokes to brighten your day.
+            Whether you're tracking for personal insight or simply want a fun way to reflect on your feelings, MoodCheck offers a thoughtful and user-friendly experience." />
         </div>
     </section>
   )
