8d9f3b3f1b2165d24eecf263de98c634b5c6da73
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-19 15:25:03 +0400

    about us commit

--- app/aboutus/page.js
+++ app/aboutus/page.js
@@ -1,16 +1,20 @@
+import BorderedTitle from '@/components/BorderedTitle';
 import MessageBox from '@/components/MessageBox';
 import React from 'react'
 
 export default function Aboutus() {
   return (
     <section className='my-10 mx-[20%] min-h-[100vh]'>
-      <div className='flex flex-col mx-12 gap-4'>
+      <BorderedTitle text="About Us" color="purple-500" posAngle/>
+      <div className='flex mx-16 max-w-[700px]  gap-8 justify-center '>
         {/* ONCLICK SHOW MORE... ANIMATIONS */}
-        <MessageBox text="Hey there! Want to know who we are?" />
-        <MessageBox response={true} text="Sure, hit me with it." />
-        <MessageBox text="We're a small team building thoughtful, creative digital tools." />
-        <MessageBox response={true} text="Nice. What's your focus?" />
-        <MessageBox text="Simple—design with purpose, and code with care. 🚀" />
+        <div className='flex flex-col gap-4'>
+          <MessageBox text="Hey there! Want to know who we are?" />
+          <MessageBox response={true} text="Sure, hit me with it." />
+          <MessageBox text="We're a small team building thoughtful, creative digital tools." />
+          <MessageBox response={true} text="Nice. What's your focus?" />
+          <MessageBox text="Simple—design with purpose, and code with care. 🚀" />
+        </div>
       </div>
 
     </section>
