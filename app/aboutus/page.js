d34669f66a43778b94580569ff195c38ea9a9ff3
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-20 16:14:45 +0400

    projects commit

--- app/aboutus/page.js
+++ app/aboutus/page.js
@@ -4,16 +4,21 @@ import React from 'react'
 
 export default function Aboutus() {
   return (
-    <section className='py-10 px-[20%] min-h-[100vh] bg-nightfall'>
-      {/* <BorderedTitle text="About Us" color="purple-500" posAngle/> */}
-      <div className='flex flex-col mx-16 max-w-[700px] gap-4'>
-        {/* ONCLICK SHOW MORE... ANIMATIONS */}
-        <MessageBox response={true} text="Hey! Who are you guys?" />
-        <MessageBox text="Hey! We’re a fresh team offering smart web solutions built around your needs." />
-        <MessageBox response={true} text="Like what kind of solutions?" />
-        <MessageBox text="Custom websites, UI/UX design, hosting, deployment - you name it." />
-        <MessageBox response={true} text="Do you stick around after launch?" />
-        <MessageBox text="Yes - and we also offer ongoing support packages to keep everything running smoothly." />
+    <section className=' min-h-[100vh] bg-nightfall bg-white '>
+      <div className='relative py-8 bg-transparent px-[20%]'>
+        <img src="https://xtratheme.com/corporate/wp-content/uploads/sites/57/2018/07/header-banner.jpg"
+        alt="Background"
+        className="absolute inset-0 w-full h-full object-cover z-0"/>
+      {/* <BorderedTitle text="About Us" color="blue-500" posAngle={false} /> */}
+        <div className='relative inset-0 z-10  flex flex-col mx-16 max-w-[700px] gap-4 '>
+          {/* ONCLICK SHOW MORE... ANIMATIONS */}
+          <MessageBox response={true} text="Hey! Who are you guys?" />
+          <MessageBox text="Hey! We’re a fresh team offering smart web solutions built around your needs." />
+          <MessageBox response={true} text="Like what kind of solutions?" />
+          <MessageBox text="Custom websites, UI/UX design, hosting, deployment - you name it." />
+          <MessageBox response={true} text="Do you stick around after launch?" />
+          <MessageBox text="Yes - and we also offer ongoing support packages to keep everything running smoothly." />
+        </div>
       </div>
 
     </section>
