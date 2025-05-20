bb7399b771060c3c81151c39d70f242ba0e19ebf
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-20 16:22:03 +0400

    projects commit

--- components/MessageBox.jsx
+++ components/MessageBox.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function MessageBox(props) {
     const {text, response} = props
   return (
-    <div className={`bg-blue-500 text-white p-4 rounded-full ` + (response? "self-start" : "self-end")}> 
+    <div className={`bg-blue-400/80 text-white p-4 rounded-full ` + (response? "self-start" : "self-end")}> 
+    {/* Response/ Start Convo Animations (response Scales?from bot-left-> top-right) (startConvo slides/grows left -> right) */}
+    {/* bg-[#94B4C1] -grayish */}
+    {/* orange-500 */}
+    {/* blue-500/400 */}
         <p className='text-sm max-w-[450px]'> {text} </p>
     </div>
   )
