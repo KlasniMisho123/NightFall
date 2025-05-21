36c31bacb66bcb706d1ce285466b27c6c98336d4
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-21 16:11:40 +0400

    about us

--- app/aboutus/page.js
+++ app/aboutus/page.js
@@ -11,14 +11,16 @@ export default function Aboutus() {
         alt="Background"
         className="absolute inset-0 w-full h-full object-cover z-0"/>
       {/* <BorderedTitle text="About Us" color="blue-500" posAngle={false} /> */}
-        <div className='relative inset-0 z-10  flex flex-col mx-16 max-w-[700px] gap-4 '>
+        <div className='relative inset-0 z-10  flex flex-col mx-16 min-h-[250px] max-w-[700px] gap-4 text-white items-center justify-center gap-8'>
           {/* ONCLICK SHOW MORE... ANIMATIONS */}
-          <MessageBox response={true} text="Hey! Who are you guys?" />
+          {/* <MessageBox response={true} text="Hey! Who are you guys?" />
           <MessageBox text="Hey! We’re a fresh team offering smart web solutions built around your needs." />
           <MessageBox response={true} text="Like what kind of solutions?" />
           <MessageBox text="Custom websites, UI/UX design, hosting, deployment - you name it." />
           <MessageBox response={true} text="Do you stick around after launch?" />
-          <MessageBox text="Yes - and we also offer ongoing support packages to keep everything running smoothly." />
+          <MessageBox text="Yes - and we also offer ongoing support packages to keep everything running smoothly." /> */}
+          <h2 className=''> About Us </h2>
+          <p> Desc </p>
         </div>
       </div>
 
