93fd6133aa1b391b6ed93ccdbfd641df41a07738
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-28 07:15:05 -0700

    meet the team commit

--- app/aboutus/page.js
+++ app/aboutus/page.js
@@ -87,13 +87,13 @@ export default function Aboutus() {
               We're a small but passionate group of creatives and builders focused on delivering thoughtful web solutions. Every project is a chance to refine our craft, learn something new, and support others in growing their ideas. We're not just here to deliver — we're here to grow with you.
             </p>
             <div className='flex mt-4'>
-               <div title='Misho' className='flex justify-center items-center h-35 w-35 rounded-full border-4 border-white z-1 relative cursor-pointer hover:z-100 hover:scale-105 '>
+               <div title='Misho' className='flex justify-center items-center h-35 w-35 rounded-full border-4 border-white z-1 relative cursor-pointer hover:z-100 hover:scale-105 transition-all transition-duration-300 '>
                 <img src='avatars/Multiavatar-smalik.png'  alt='misho_avatar'/>
                </div>
-               <div title='Alika' className='flex justify-center items-center h-35 w-35 bg-red-500 rounded-full border-4 border-white z-2 relative ml-[-15px] cursor-pointer hover:z-100 hover:scale-105 '>
+               <div title='Alika' className='flex justify-center items-center h-35 w-35 bg-red-500 rounded-full border-4 border-white z-2 relative ml-[-15px] cursor-pointer hover:z-100 hover:scale-105 transition-duration-300 '>
                 <img src='avatars/Multiavatar-Alik.png'  alt='misho_avatar'/>
                </div>
-               <div title='DummyTeamMember' className='flex justify-center items-center h-35 w-35 bg-red-500 rounded-full border-4 border-white z-3 relative ml-[-15px] cursor-pointer hover:z-100 hover:scale-105 '>
+               <div title='DummyTeamMember' className='flex justify-center items-center h-35 w-35 bg-red-500 rounded-full border-4 border-white z-3 relative ml-[-15px] cursor-pointer hover:z-100 hover:scale-105 transition-duration-300 '>
                 <img src='avatars/Multiavatar-an.png'  alt='misho_avatar'/>
                </div>
                <div className="flex justify-center items-center h-35 w-35 bg-gray-300 rounded-full border-4 border-white relative z-30 ml-[-15px] select-none">
