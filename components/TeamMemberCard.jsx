e5f46f822a37d399605afd052806fbbf01f9cfc1
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-29 06:34:56 -0700

    select Team Member

--- components/TeamMemberCard.jsx
+++ components/TeamMemberCard.jsx
@@ -12,8 +12,8 @@ export default function TeamMemberCard(props) {
     // Strong Sides ??
     // Fun Facts ??
   return (
-    <div className={`flex flex-col items-start rounded text-black transition-all duration-500 ease-in-out pt-6 px-4 team-member-card overflow-hidden`}>
-    <div className="flex flex-col items-center bg-white p-6 rounded-xl shadow-lg w-full max-w-md relative">
+    <div className={`flex flex-col items-start rounded text-black transition-all duration-500 ease-in-out pt-6 team-member-card overflow-hidden `}>
+    <div className="flex flex-col items-center bg-white p-6 rounded-xl shadow-lg w-full max-w-md relative  border-2">
 
         <i
         className="fa-solid fa-xmark text-xl transition-all duration-300 text-red-500 hover:text-red-400 hover:scale-105 cursor-pointer absolute top-4 right-4 "
@@ -42,7 +42,7 @@ export default function TeamMemberCard(props) {
 
         <div className="mt-5 w-full">
         <h3 className="text-md font-semibold text-gray-700 mb-2">Strong Sides
-            <i className="fa-solid fa-dumbbell ml-2 text-blue-600 text-lg"></i>
+            <i className="fa-solid fa-dumbbell ml-2 text-gray-700 text-lg"></i>
         </h3>
         <ul className="text-sm text-gray-600 list-disc list-inside space-y-1">
             <li>Pixel-perfect design implementation</li>
@@ -62,20 +62,32 @@ export default function TeamMemberCard(props) {
         </ul>
         </div>
 
-        <div className="flex gap-4 mt-6">
+        <div className="flex self-start gap-4 mt-8">
         <a
-            href="mailto:misho@example.com"
-            className="bg-red-500 hover:bg-red-400 text-white px-4 py-2 rounded-full text-sm transition"
+            title="mailto:misho@example.com"
+            className="bg-red-500 hover:bg-red-400 text-white px-4 py-2 rounded-full text-sm transition cursor-not-allowed"
         >
-            Email
+            <i className="fa-solid fa-envelope text-xl text-white"></i>
         </a>
+
         <a
             href="https://linkedin.com/in/misho"
+            title="Linkedin"
             target="_blank"
             rel="noopener noreferrer"
             className="bg-blue-600 hover:bg-blue-500 text-white px-4 py-2 rounded-full text-sm transition"
         >
-            LinkedIn
+            <i className="fa-brands fa-linkedin text-xl "></i>
+        </a>
+
+        <a
+            href="https://linkedin.com/in/misho"
+            title="Github"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-gray-900 hover:bg-gray-700 text-white px-4 py-2 rounded-full text-sm transition"
+        >
+            <i className="fa-brands fa-github text-xl "></i>
         </a>
         </div>
     </div>
