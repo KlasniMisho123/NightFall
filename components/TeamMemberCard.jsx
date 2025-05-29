4aac1cbc6cdd36ca0b13d8e9953897cfb0ecf0d9
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-29 05:02:07 -0700

    select Team Member

--- components/TeamMemberCard.jsx
+++ components/TeamMemberCard.jsx
@@ -5,7 +5,7 @@ export default function TeamMemberCard(props) {
   return (
     <div className={`flex flex-col items-center max-w-[800px] w-full bg-white rounded text-black transition-all duration-500 ease-in-out pt-6 team-member-card
      overflow-hidden ${selectedTeamMember ? " max-h-[40vh] " : " max-h-[0vh] "} `}>
-        <i className="fa-solid fa-xmark text-xl transition-all duration-300 text-red-500 relative z-5 hover:text-red-400 hover:scale-105 cursor-pointer absolute self-end "
+        <i className="fa-solid fa-xmark text-xl transition-all duration-300 text-red-500 relative z-5 hover:text-red-400 hover:scale-105 cursor-pointer absolute self-end team-member-xmark "
         onClick={() => selectTeamMember("")}></i>
         <div className={`flex justify-center items-center h-35 w-35 border-white rounded-full border-4 z-3 relative ml-[-15px] cursor-pointer
             transition-duration-300 `}>
