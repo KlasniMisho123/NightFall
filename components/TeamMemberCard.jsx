93b3a545c05d44a82b5fba609c3444ad8cd4f4a7
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-29 04:26:19 -0700

    select Team Member

--- components/TeamMemberCard.jsx
+++ components/TeamMemberCard.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 export default function TeamMemberCard(props) {
     const {selectedTeamMember} = props
   return (
-    <div className={`flex flex-col items-center h-[40vh] max-w-[800px] w-full bg-white rounded p-4 text-black transition-all duration-500 ease-in-out pt-6
-     overflow-hidden ${selectedTeamMember ? " max-h-[40vh] " : " max-h-[0vh] " } `}>
+    <div className={`flex flex-col items-center max-w-[800px] w-full bg-white rounded text-black transition-all duration-500 ease-in-out pt-6 team-member-card
+     overflow-hidden ${selectedTeamMember ? " max-h-[40vh] " : " max-h-[0vh] "} `}>
+        <i className="fa-solid fa-xmark text-lg transition-all duration-300 text-red-500 relative z-5 hover:text-red-400 hover:scale-105 "></i>
         Selected: {selectedTeamMember}
       </div>
   )
