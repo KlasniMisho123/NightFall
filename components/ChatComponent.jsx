2250f7be10154c9adf63775acf178eb2aa040738
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-30 05:09:05 -0700

    team member close animation

--- components/ChatComponent.jsx
+++ components/ChatComponent.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import MessageBox from './MessageBox'
 
 export default function ChatComponent() {
+    const [selectedMessage, setSelectedMessage] = useState(null)
+
+    function selectMessage(messageIndex) {
+      if(messageIndex == selectedMessage ) {
+        setSelectedMessage(null)
+      } else {
+        setSelectedMessage(messageIndex)
+      }
+    }
+
   return (
     <div className='flex flex-col min-w-[600px] max-w-[700px] justify-self-center py-12 gap-4'> 
-            {/* ONCLICK SHOW MORE... ANIMATIONS */}
-        <MessageBox response={true} text="Hey! Who are you guys?" />
-        <MessageBox text="Hey! We’re a fresh team offering smart web solutions built around your needs." />
-        <MessageBox response={true} text="Like what kind of solutions?" />
-        <MessageBox text="Custom websites, UI/UX design, hosting, deployment - you name it." />
-        <MessageBox response={true} text="Do you stick around after launch?" />
-        <MessageBox text="Yes - and we also offer ongoing support packages to keep everything running smoothly." />
+        {/* ONCLICK SHOW MORE... ANIMATIONS */}
+        <MessageBox text="Hey! Who are you guys?" clickIndex={1} selectMessage={selectMessage} />
+        <MessageBox response={true} text="Hey! We’re a fresh team offering smart web solutions built around your needs." />
+        <MessageBox text="Like what kind of solutions?" clickIndex={2} selectMessage={selectMessage} />
+        <MessageBox response={true} text="Custom websites, UI/UX design, hosting, deployment - you name it." />
+        <MessageBox text="Do you stick around after launch?" clickIndex={3} selectMessage={selectMessage} />
+        <MessageBox response={true} text="Yes - and we also offer ongoing support packages to keep everything running smoothly." />
     </div>
   )
 }
--- components/MessageBox.jsx
+++ components/MessageBox.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 export default function MessageBox(props) {
     const {text, response} = props
   return (
-    <div className={`bg-blue-400/80 text-white p-4 rounded-full ` + (response? "self-start" : "self-end")}> 
-    {/* Response/ Start Convo Animations (response Scales?from bot-left-> top-right) (startConvo slides/grows left -> right) */}
-    {/* bg-[#94B4C1] -grayish */}
-    {/* orange-500 */}
-    {/* blue-500/400 */}
+    <div className={`bg-blue-400/80 text-white p-4 rounded-full shadow-md ` + (response? "self-end" : "self-start")}> 
         <p className='text-sm max-w-[450px]'> {text} </p>
     </div>
   )
