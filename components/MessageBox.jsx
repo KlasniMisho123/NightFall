d3de6821b9e104b98a3a53a8b6c1cb16fdbf137b
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-05-31 06:32:39 -0700

    nav commit

--- components/MessageBox.jsx
+++ components/MessageBox.jsx
@@ -14,9 +14,9 @@ export default function MessageBox(props) {
     className={` bg-blue-400/80 text-white p-4 rounded-full shadow-md transition-all duration-300
      ${response ? ' self-end response-message ' : ' self-start start-message '}
      ${clickIndex !== null ? ' cursor-pointer hover:bg-blue-400 shadow-none ' : ''}
-     ${!selectedMessage?.includes(clickIndex) && clickIndex !== null ? " slide-message " : " "}`}
+     ${!selectedMessage?.includes(clickIndex) && clickIndex !== null ? " slide-message " : ""}`}
     onClick={clickIndex !== null ? handleClick : undefined}>
-        <p className='text-sm max-w-[450px]'> {text} </p>
+        <p className='text-sm max-w-[450px]'> {text} {selectedMessage}</p>
     </div>
   )
 }
