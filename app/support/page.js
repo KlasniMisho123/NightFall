a8e89c0dceaf62cdb56e5b5c3699941ba4bcf93e
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-01 03:51:11 -0700

    create new project commit

--- app/support/page.js
+++ app/support/page.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default function Support() {
+  // Socials Element
+  // Visual
   return (
     <div>Support</div>
   )
--- components/OrderProject.jsx
+++ components/OrderProject.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function OrderProject() {
   return (
-    <div className=' self-center my-4 p-4 bg-white rounded-lg '>
-        OrderProject Ui Element
+    <div className='flex justify-between my-8 p-4 bg-white rounded-lg '>
+      <div className=''>
+        <h4> Want To Create Website for Your Buisness/Startup/other?</h4>
+        <h5> Let Us Know </h5>
+      </div>
+      <button className=' self-start '> <i className="fa-solid fa-plus text-blue-500"></i>  Create Now</button>
     </div>
   )
 }
