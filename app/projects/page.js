b18e1a4bd61ea39c5d6642b819176773ae1ba4cf
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-02 05:42:49 -0700

    build error fix

--- app/projects/page.js
+++ app/projects/page.js
@@ -24,7 +24,6 @@ export default function Projects() {
             <h1 className={`text-white text-4xl my-4 md:text-6xl lg:text-7xl font-bold mt-2 whitespace-nowrap text-center leading-tight ` + urbanist.className} >Sharing Our Experience <br/> <span className='text-blue-500/80 '> With You  </span></h1>
             <p className={"text-gray-400 text-sm max-w-md  mt-3 leading-relaxed lg:max-w-2xl md:text-base text-center " + inter.className} >Interested in what we've worked on and how we've contributed? Take a look at our past projects and see what we've been up to.</p>
           </div>
-          {/* Div For Projects */}
         </section>
       </div>
       <div className='px-4 md:px-14 lg:px-28 '>
