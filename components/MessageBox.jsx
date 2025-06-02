43d75d2a756d1620afce4a32bbd1f8e42364b439
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-02 03:00:27 -0700

    messages debug commit

--- components/MessageBox.jsx
+++ components/MessageBox.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function MessageBox(props) {
     const {text, response, clickIndex=null, selectMessage, selectedMessage} = props
+
 
     const handleClick = () => {
       if (clickIndex !== null) {
         selectMessage(clickIndex);
       }
     };
+    useEffect(()=>{
+      const isSelected = selectedMessage?.includes(clickIndex);
+      console.log("includes?: ",clickIndex,"-", isSelected)
+    },[selectedMessage]);
 
   return (
     <div 
-    className={`bg-blue-400/80 text-white p-5  rounded-full shadow-md transition-all duration-300
+    className={`bg-blue-400/80  p-5  rounded-full shadow-md transition-all duration-300
      ${response ? ' self-end response-message ' : ' self-start '}
      ${clickIndex !== null ? ' cursor-pointer hover:bg-blue-400 shadow-none bg-transparent border-blue-500 border-2 ' : ''}
-     ${selectedMessage?.includes(clickIndex)? "bg-blue-400/80 " : " "}
+     ${selectedMessage?.includes(clickIndex)? "bg-blue-400/80 text-red-500 " : " "}
      `}
     onClick={clickIndex !== null ? handleClick : undefined}>
         <p className='text-sm max-w-[500px] mx-2'> {text} {selectedMessage} -- {clickIndex} -- {selectedMessage?.includes(clickIndex)} </p>
