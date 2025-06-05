9c35b1d49abc532f4a504a1ccb2d77dd53679bec
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-05 06:08:46 -0700

    projects title res commit

--- app/projects/page.js
+++ app/projects/page.js
@@ -14,10 +14,10 @@ export default function Projects() {
       style={{
         backgroundImage: "url('projects-bg.avif')",
       }}>        
-        <section className='my-10 mx-[20%] hero-stats-load '>
+        <section className='my-10 mx-[10%] md:mx-[20%] hero-stats-load '>
           <BorderedTitle text="Our Projects" color="blue-500" angle={5}/>
           <div className='text-white my-6'>
-            <h1 className={`text-white text-4xl my-4 md:text-6xl lg:text-7xl font-bold mt-2 whitespace-nowrap text-center leading-tight ` + urbanist.className} >Sharing Our Experience <br/> <span className='text-blue-500/80 '> With You  </span></h1>
+            <h1 className={`text-white text-4xl my-4 sm:text-5xl md:text-6xl lg:text-7xl font-bold mt-2 sm:whitespace-nowrap text-center leading-tight ` + urbanist.className} >Sharing Our Experience <br/> <span className='text-blue-500/80 '> With You  </span></h1>
             <p className={"text-gray-400 text-sm max-w-md  mt-3 leading-relaxed lg:max-w-2xl md:text-base text-center " + inter.className} >Interested in what we&apos;ve worked on and how we&apos;ve contributed? Take a look at our past projects and see what we&apos;ve been up to.</p>
           </div>
         </section>
