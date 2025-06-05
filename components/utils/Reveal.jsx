ff121016f26cbd42f3bda6f0c0b3cddc55f101dc
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-05 03:04:47 -0700

    projects reveal animation

--- components/utils/Reveal.jsx
+++ components/utils/Reveal.jsx
@@ -1,33 +1,33 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { motion, useAnimation, useInView } from "motion/react"
+import { motion, useAnimation, useInView } from "framer-motion";
 
 export default function Reveal( {children, width = "fit-content", projectIndex=2}, props) {
     const ref = useRef(null)
     const isInView = useInView(ref, {once: false})
-    
     const mainControls = useAnimation();
-    const [initialHiddenVariant, setInitialHiddenVariant] = useState({ opacity: 0, x: 0 });
+    
+    const isOdd = projectIndex % 2 !== 0;
+    const hiddenVariant = { opacity: 0, x: isOdd ? -200 : 200 };
+    const delayVal =  isOdd ? 0.25 : 0.40; 
 
     useEffect(() => {
         if (isInView) {
-            const isOdd = projectIndex % 2 !== 0;
-            setInitialHiddenVariant({ opacity: 0, x: isOdd ? -200 : 200 });
             mainControls.start("visible");
         } else {
             mainControls.start("hidden");
         }
-    }, [isInView, projectIndex, mainControls]);
+    }, [isInView, mainControls]);
 
 return (
     <div ref={ref} style={{ position: "relative", width, overflow: "hidden" }}>
         <motion.div
         variants={{
-            hidden: isOddIndex,
+            hidden: hiddenVariant,
             visible: {opacity:1, x:0 },
         }}
         initial="hidden"
         animate={mainControls}
-        transition={{duration: 0.25, delay: 0.25, ease:"easeIn"}}
+        transition={{duration: 0.25, delay: delayVal, ease:"easeIn"}}
         > {children} </motion.div>
     </div>
   )
