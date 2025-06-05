467853b8055a27edd827c582e6874c1d5142f5aa
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-05 02:55:59 -0700

    projects reveal animation

--- components/utils/Reveal.jsx
+++ components/utils/Reveal.jsx
@@ -1,26 +1,36 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { motion, useAnimation, useInView } from "motion/react"
 
 export default function Reveal( {children, width = "fit-content", projectIndex=2}, props) {
     const ref = useRef(null)
     const isInView = useInView(ref, {once: true})
+    const {isOddIndex, setIsOddIndex} = useState({}) 
 
     const mainControls = useAnimation();
 
-
+    useEffect(() => {
+        if(isInView) {
+             if(projectIndex % 2 == 0){
+                setIsOddIndex({opacity:0, x:200 })
+            } else {
+                setIsOddIndex({opacity:0, x:-200 },)
+            }
+            mainControls.start("visible")
+        }
+    },[])
+    
     useEffect(() => {
         if(isInView) {
             mainControls.start("visible")
-            console.log(projectIndex)
         }
-
     },[isInView])
+
 
 return (
     <div ref={ref} style={{ position: "relative", width, overflow: "hidden" }}>
         <motion.div
         variants={{
-            hidden: {opacity:0, x:200 },
+            hidden: isOddIndex,
             visible: {opacity:1, x:0 },
         }}
         initial="hidden"
