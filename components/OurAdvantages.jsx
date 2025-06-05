dce004ca3a1f7b87a04a40750924af7d76352247
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-05 05:37:59 -0700

    hero res commit

--- components/OurAdvantages.jsx
+++ components/OurAdvantages.jsx
@@ -40,7 +40,7 @@ const advantageCardArray = [
 
 export default function OurAdvantages() {
   return (
-    <div className='py-8 border border-gray-200 flex flex-col justify-center items-center gap-4 min-w-[430px] overflow-hidden '>
+    <div className='py-8 border border-gray-200 flex flex-col justify-center items-center gap-4 min-w-[250px] overflow-hidden '>
         <h1 className={'text-center text-lg lg:text-2xl textGradient text-white ' + fugaz.className}> Our Advantages </h1>
         <div className='flex flex-row py-4 overflow-hidden px-8 '> 
         {advantageCardArray.map((card, index) => ( 
