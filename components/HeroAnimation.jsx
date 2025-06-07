3880a4b1e3e5bf97bcf96e4131a089ec2348b98f
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-07 07:28:06 -0700

    HeroAnimation

--- components/HeroAnimation.jsx
+++ components/HeroAnimation.jsx
@@ -3,9 +3,21 @@ import Image from 'next/image';
 
 export default function HeroAnimation() {
   return (
-    <section className='min-w-0 sm:min-w-[20%] md:min-w-[25%] text-white main-hero-load flex items-center'>
-      <div className='flex flex-col items-center p-8'>
-        <div className='flex justify-center gap-2'>
+    <div className='w-full h-full '>
+      {/* <Image
+        src="https://i.pinimg.com/736x/1b/c5/3e/1bc53e53aa055c0247e30714a874adb5.jpg" 
+        alt="Astronaut on the Moon"
+        className='w-[100%] h-[100%] object-cover '
+        width={100}
+        height={100} /> */}
+        <img
+         src="https://i.pinimg.com/736x/1b/c5/3e/1bc53e53aa055c0247e30714a874adb5.jpg" 
+        />
+    </div>
+
+    // <section className='min-w-0 sm:min-w-[20%] md:min-w-[25%] text-white main-hero-load flex items-center'>
+    //   <div className='flex flex-col items-center p-8'>
+      /* <div className='flex justify-center gap-2'>
           <img
             src='sun.png'
             alt='Sun icon'
@@ -21,14 +33,9 @@ export default function HeroAnimation() {
           src='Mountains.png'
           alt='Mountain landscape'
           className='mt-4 object-cover rise-up'
-        />
-      </div>
-        {/* <Image
-          src="/astronaut-moon.svg" 
-          alt="Astronaut on the Moon"
-          className=''
-          width={100}
-          height={100} /> */}
-    </section>
+        /> */
+    //   </div>
+    // </section>
+    
   )
 }
