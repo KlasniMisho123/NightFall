4f13825b8da63e9bc458e7e4a9d7e67238782e5d
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-06-09 08:33:07 -0700

    not foundpage commit

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,7 +1,15 @@
+import Link from 'next/link';
 import React from 'react'
 
-export default function not-found() {
+export default function NotFound() {
   return (
-    <div>not-found</div>
-  )
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white text-center px-4">
+      <h1 className="text-6xl font-bold mb-4">404</h1>
+      <p className="text-2xl mb-2">Page Not Found</p>
+      <p className="text-gray-400 mb-6">The page you’re looking for doesn’t exist or was moved.</p>
+      <Link href="/" className="px-6 py-2 bg-blue-600 hover:bg-blue-500 transition rounded-full text-white font-semibold cursor-pointer ">
+        Go Back Home
+      </Link>
+    </div>
+  );
 }
